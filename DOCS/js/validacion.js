e42341eb41729d477522fca748c3357c3ff335b7
Lukas <lukasmeza484@gmail.com>
2021-04-30 13:17:12 -0400

    Validacion formulario

--- DOCS/js/validacion.js
+++ DOCS/js/validacion.js
@@ -1,80 +1,51 @@
-// objeto.metodo(json)
+$(document).ready(function () {
+    $("#formulario1").validate({
+        rules: {
+            txtNombre: {
+                required: true,
+                minlength: 3
 
-$("#formulario1").validate({
-    rules: {
-        "txtNombre": {
-            required: true,
-            minlength: 3,
-            text: true
+            },
+            txtApellido: {
+                required: true,
+                minlength: 3
 
-        },
-        "txtApellido": {
-            required: true,
-            minlength: 3,
-            text: true
+            },
+            txtEmail: {
+                required: true,
+                email: true
+            },
+            txtTelefono: {
+                required: true,
+                max: 999999999
+            },
+            txtRepetirTelefono: {
+                required: true,
+                equalTo: '#id_txtTelefono'
+            },
+            txtFechaNacimiento:{
+                required: true,
+                date: true
+            },
+            txtContrasena: {
+                required: true,
+                minlength: 7
+            },
+            txtRepetirContrasena: {
+                required: true,
+                equalTo: '#id_txtContrasena'
+            },
+            categorias:{
+                required: true
+            },
 
-        },
-        "txtEmail": {
-            required: true,
-            email: true
-        },
-        "txtTelefono": {
-            required: true,
-            max: 999999999
-        },
-        "txtRepetirTelefono": {
-            required: true,
-            equalTo: '#id_txtTelefono'
-        },
-        "txtContrasena": {
-            required: true,
-            password: true
-        },
-        "txtRepetirContrasena": {
-            required: true,
-            equalTo: '#id_txtContrasena'
-        }
-
-    }, // --> Fin de reglas
-    messages: {
-        "txtContrasena": {
-            required: 'Ingrese su contraseña!!!'
-
-        },
-        "txtRepetirContrasena": {
-            required: 'Repita su contraseña!!!',
-            equalTo: 'La contraseña no es igual a la primera!!!!'
-
-        },
-        "txtDireccion": {
-            required: 'Ingrese su direccion!!!'
-
-        },
-        "txtEmail": {
-            required: 'Ingrese email!!!',
-            email: 'No cumple formato'
-        },
-        "txtTelefono": {
-            required: 'Ingrese Telefono!!!',
-            min: 999999999
-        },
-        "txtRepetirTelefono": {
-            required: 'Repita su telefono!!',
-            equalTo: ' deben ser iguales!!!!'
-        },
-        "txtNombre": {
-            required: 'Ingrese sus nombres!!!',
-
-        },
-        "txtApellido": {
-            required: 'Ingrese sus apelllidos!!!',
+            sexo:{
+                required: true
+            }
 
 
         },
-        "txtFechaNacimiento": {
-            required: 'Debe ingresar fecha de nacimiento!!!',
-            date: 'La fecha maxima permitida es 31-12-2010'
-        }
-    } //-->Fin de mensajes
+
+    });
 
 });
