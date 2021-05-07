8a06539f5b96ddb9d9f50551bc0ae8333456030e
JORGE ALFARO <jorgeandresalfarocastro@gmail.com>
2021-05-07 10:59:32 -0400

    servicio web parte 1

--- DOCS/js/agregar_formulario.js
+++ DOCS/js/agregar_formulario.js
@@ -1,7 +1,15 @@
 $(function () {
   $("#form-check").click(function (event) {
     event.preventDefault();
+    var Run = $("#id_rut").val();
+    var Nombres = $("#id_txtNombre").val();
+    var mail = $("#id_txtEmail").val();
+    var pass = $("#id_txtRepetirContrasena").val();
     alert("Registro completado");
+     var fila = '<tr><th>'+Run+'</th><th>'+Nombres+'</th><th>'+mail+'</th><th>'+pass+'</th>'
+     $('#tablaprueba>tbody').append(fila);
+    var documento = $("#tablaprueba");
+   
 
     });
   //   cierre del click de submint
