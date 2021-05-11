04863861350e938fd00e2b0d071bfafe87ac2cc5
Franco Sandoval <francosandoval003@gmail.com>
2021-05-11 13:41:33 -0400

    Documentacion y Reparaciones varias

--- DOCS/js/servico_web_datos.js
+++ DOCS/js/servico_web_datos.js
@@ -2,8 +2,8 @@ $("#btn-obtener").click(function () {
         $.get("https://imdb-api.com/en/API/Top250Movies/k_29bfmsvl/",
         function(data){
             $.each(data.items,function(i, item){
-                var fila = "<tr><td>"+item.title.id+"</td><td>"+item.year +
-                            "</td><td><img src='"+item.image+"'>" +
+                var fila = "<tr><td>"+i+"</td><td>"+item.title+"</td><td>"+item.year +
+                            "</td><td><img src='"+item.image+" width='100' height='200''>" +
                             "</td><td>"+item.imDbRating+"</td></tr>";
 
                 $("#tabla-categorias").append(fila);   
